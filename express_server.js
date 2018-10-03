3112ba449d3812622f6c99d5045868bf0f5742c7
Sherlaine <sherlaine.lau@gmail.com>
2018-10-03 15:33:34 +0000

    added the random code generalizer

--- express_server.js
+++ express_server.js
@@ -3,19 +3,21 @@ var app = express();
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-function generateRandomString(digits) {
-     var text = '';
-     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
-     for (var i = 0; i < digits; i++){
-         text += possible.charAt(Math.floor(Math.random() * possible.length));
-     }
-  
-     return text;
-  }
-  console.log(generateRandomString(6));
+function generateRandomString() {
+    var text = '';
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+
+    for (var i = 0; i < 6; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+console.log(generateRandomString());
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -48,19 +50,23 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
     redirect: " "
-  });
+});
 
 app.post("/urls", (req, res) => {
-    console.log(req.body);  // debug statement to see POST parameters
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
+    console.log(req.body); // debug statement to see POST parameters
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/u/:shortURL", (req, res) => {
+    // let longURL = ...
+    res.redirect(longURL);
+});
 
 app.get("/urls/:id", (req, res) => {
     console.log("inside id");
-    let templateVars = { longURL: urlDatabase[req.params.id]};
+    let templateVars = {
+        longURL: urlDatabase[req.params.id]
+    };
     res.render("urls_show", templateVars);
     console.log(templateVars)
-  });
-
-  
-  
+});
