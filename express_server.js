d25eefedd523c3a2cc12149387c9d73232a90e31
Sherlaine <sherlaine.lau@gmail.com>
2018-10-04 02:23:09 +0000

    added the cookie function in the urls page

--- express_server.js
+++ express_server.js
@@ -2,13 +2,18 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({
     extended: true
+    
 }));
 
+//this if for our random string generator
 function generateRandomString() {
-      //Solution from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    //Solution from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
     var text = '';
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
 
@@ -18,7 +23,6 @@ function generateRandomString() {
 
     return text;
 }
-// console.log(generateRandomString());
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -52,34 +56,18 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+//on our main url page, generates a new random id when we create a different page
 app.post("/urls", (req, res) => {
     var id = generateRandomString();
     urlDatabase[id] = req.body.longURL;
-
-    res.redirect('/urls/'+id);
-
+    res.redirect('/urls/' + id);
     console.log(urlDatabase);
-    // console.log(req.body); // debug statement to see POST parameters
-    // res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
-// deletes the link page
-app.post("/urls/:id/delete", (req, res) =>{
-    delete urlDatabase[req.params.id] 
+// deletes the linked page
+app.post("/urls/:id/delete", (req, res) => {
+    delete urlDatabase[req.params.id]
     res.redirect('/urls/');
-})
-
-
-app.post("/urls/:id", (req, res) =>{
-    res.redirect('/urls/new');
-})
-
-app.post("/urls/:id/update", (req, res) =>{
-    var shortURL = req.params.id 
-    urlDatabase[shortURL] = req.body.newLongURL;
-
-    res.redirect('/urls');
-    //we are updating the the request link long URL 
 })
 
 app.get("/urls/:id", (req, res) => {
@@ -91,6 +79,27 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
     console.log(templateVars)
 });
+
+//cookie function
+
+app.post("/login", (req, res) => {
+    res.cookie("username", req.body.username);
+    res.redirect('/urls');
+})
+app.post("/urls/:id", (req, res) => {
+    res.redirect('/urls/new');
+})
+
+app.post("/urls/:id/update", (req, res) => {
+    var shortURL = req.params.id
+    urlDatabase[shortURL] = req.body.newLongURL;
+
+    res.redirect('/urls');
+    //we are updating the the request link long URL 
+})
+
+
+
 
 app.get("/u/:shortURL", (req, res) => {
 
